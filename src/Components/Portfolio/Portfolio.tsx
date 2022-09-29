3e3ddf6f29081b20648a515492ed1832fabd28f0
yousef <youseff2000q@gmail.com>
2022-09-29 07:43:01 -0700

    edit filter gallery

--- src/Components/Portfolio/Portfolio.tsx
+++ src/Components/Portfolio/Portfolio.tsx
@@ -4,54 +4,41 @@ import React, { useRef, useState, useEffect, useMemo } from "react";
 import axios, { AxiosResponse } from "axios";
 import "./Portfolio.css";
 export default function Portfolio() {
-  const [filterClass, setFilterClass] = useState("");
+  // const [filterClass, setFilterClass] = useState("all");
+  const [tag, setTag] = useState("all");
   const [info, setInfo] = useState([]);
-  const showInfo = () => {
-    return (
-      <>
-        {info.map((item: any) => {
-          return (
-            <div className={item.type} key={item.id}>
-              <div>
-                <img src={item.image} className="w-100" alt="" />
-              </div>
-            </div>
-          );
-        })}
-      </>
-    );
-  };
+
   // remove active from all and put it on target and filter gallery
+  // start filterGallery
   const lis = document.querySelectorAll(
       "#Portfolio ul li"
     ) as NodeListOf<HTMLLIElement>,
     div = document.querySelectorAll(
       "#Portfolio .row >div"
     ) as NodeListOf<HTMLDivElement>;
-  // start filterGallery
   const filterGallery = () => {
-    console.log(filterClass);
     lis.forEach((li) => {
       li.addEventListener("click", (e: MouseEvent) => {
         //remove class active from all
         lis.forEach((li) => {
           li.classList.remove("active");
         });
-        // declare the element (like extends the interface)
+        //       // declare the element (like extends the interface)
         const target = e.target as HTMLElement;
         // add class active to target
         target.classList.add("active");
-        // filter gallery give the class types from dataset of e.target
-        div.forEach((item: any) => {
-          item.style.cssText = "width:0; padding:0";
-        });
-        document.querySelectorAll(`${filterClass}`).forEach((item: any) => {
-          console.log(item);
-          item.style.cssText = "opacity:1";
-        });
+        //       // filter gallery give the class types from dataset of e.target
+        // div.forEach((item: HTMLElement) => {
+        //   item.style.cssText = "width:0; padding:0";
+        // });
+        // console.log(target.dataset.type);
+        // document.querySelectorAll(`${filterClass}`).forEach((item: any) => {
+        //   // console.log(item);
+        //   item.style.cssText = "opacity:1";
+        // });
       });
     });
-    console.log("filterGallery", "filterGallery");
+    //   console.log("filterGallery", "filterGallery");
   };
 
   // end filterGallery
@@ -94,22 +81,25 @@ export default function Portfolio() {
     axios
       .get("js/data.json")
       .then((res: AxiosResponse) => {
-        setInfo(res.data.portfolio);
+        tag == "all"
+          ? setInfo(res.data.portfolio)
+          : setInfo(res.data.portfolio.filter((item: any) => item.tag == tag));
       })
       .catch((err: Error) => {
         console.log(err);
       });
+    // console.log(info);
     // filterGallery();
     // lis[0].click();
+    // console.log(tag, "tag");
     filterGallery();
-    // filterGallery();
     // popup();
-
+    // showInfo();
     // console.log("filterGallery", "filterGallery");
     // randomNum();
     // popup();
     // put [] in the future and fix the problem
-  }, [filterClass]);
+  }, [tag]);
   return (
     <div id="Portfolio" className="py-5">
       <div className="container-fluid text-center">
@@ -117,43 +107,68 @@ export default function Portfolio() {
         <ul className="list-unstyled my-3 d-flex align-items-center justify-content-center flex-sm-row flex-column">
           <li
             className="text-uppercase py-2 px-4 active"
-            onClick={() => setFilterClass(".all")}
+            onClick={() => setTag("all")}
             data-type=".all"
           >
             all
           </li>
           <li
             className="text-uppercase py-2 px-4"
-            onClick={() => setFilterClass(".html")}
+            onClick={() => setTag("html")}
             data-type=".html"
           >
             html
           </li>
           <li
             className="text-uppercase py-2 px-4"
-            onClick={() => setFilterClass(".css")}
+            onClick={() => setTag("css")}
             data-type=".css"
           >
             css
           </li>
           <li
             className="text-uppercase py-2 px-4"
-            onClick={() => setFilterClass(".js")}
+            onClick={() => setTag("js")}
             data-type=".js"
           >
             js
           </li>
           <li
             className="text-uppercase py-2 px-4"
-            onClick={() => setFilterClass(".react")}
+            onClick={() => setTag("react")}
             data-type=".react"
           >
             react
           </li>
         </ul>
-        <div className="gallery row">{showInfo()}</div>
+        <div className="gallery row">
+          {info.map((item: any) => {
+            return (
+              <div className={item.type} key={item.id}>
+                <div>
+                  <img src={item.image} className="w-100" alt="" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <img src="/public/images/Portfolio/portfolio-img8.jpg" alt="" />
     </div>
   );
 }
+// const showInfo = () => {
+//   return (
+//     <>
+//       {info.map((item: any) => {
+//         return (
+//           <div className={item.type} key={item.id}>
+//             <div>
+//               <img src={item.image} className="w-100" alt="" />
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </>
+//   );
+// };
